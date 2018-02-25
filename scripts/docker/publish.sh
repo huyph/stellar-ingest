adfd4332447a76764c859a85a462bddf30ac8f02
Filippo Ammazzalorso <Filippo.Ammazzalorso@data61.csiro.au>
2018-02-25 18:12:47 +1100

    Renamed script, updated travis and removed useless files.

--- scripts/docker/publish.sh
+++
@@ -1,7 +0,0 @@
-INGEST_VERSION=$(cd ../..; lein pom &> /dev/null && mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
-
-docker login
-
-# docker push registry-host:5000/namespace/repository-name
-docker push data61/stellar-ingest:$INGEST_VERSION
-docker push data61/stellar-ingest:latest
