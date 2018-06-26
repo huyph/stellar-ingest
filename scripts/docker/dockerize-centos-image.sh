16e8710b807a259ad8860cc57ad85c4e02ee36dd
TheDen <deniskhoshaba@gmail.com>
2018-06-26 14:20:43 +1000

    add relative basedir path

--- scripts/docker/dockerize-centos-image.sh
+++ scripts/docker/dockerize-centos-image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build -t data61/stellar-ingest:snapshot-centos -f Dockerfile.centos ../../
+BASEDIR=$(dirname "$0")
+
+docker build -t data61/stellar-ingest:snapshot-centos -f "${BASEDIR}/Dockerfile.centos" "${BASEDIR}/../../"
