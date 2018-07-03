f0858fa29edb7b1aceb4993685e98cde6733ba39
TheDen <deniskhoshaba@gmail.com>
2018-07-03 14:53:19 +1000

    add pipeline.yml + script

---
+++ .buildkite/script.sh
@@ -0,0 +1,31 @@
+INGEST_VERSION=$(lein project-version 2> /dev/null)
+INGEST_TAG=$(git describe --tags|head -1 2> /dev/null)
+
+
+lein cloverage --coveralls && \
+  curl -F 'json_file=@target/coverage/coveralls.json' "https://coveralls.io/api/v1/jobs";
+
+echo ""; echo ""; echo "branch: $BUILDKITE_BRANCH, version: $INGEST_VERSION, PR: $BUILDKITE_PULL_REQUEST, travis_tag: $TRAVIS_TAG, ingest_tag: $INGEST_TAG"
+
+	 if [ "$TRAVIS_EVENT_TYPE" == push -a "$TRAVIS_BRANCH" == "devel" ]; then
+		 if [[ $INGEST_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]]; then
+			 echo "Publishing snapshot from devel branch.";
+			 lein deploy;
+			 lein deploy-uberjar;
+			 #./scripts/docker/dockerize.sh;
+		 else
+			 echo "ERROR invalid snapshot version."; exit 1;
+		 fi;
+	 fi
+
+#	# deploy release
+#	if [ "$TRAVIS_EVENT_TYPE" == push -a -n "$TRAVIS_TAG" ]; then
+#		if [ "$TRAVIS_TAG" == "v$INGEST_VERSION" ]; then
+#			echo "Publishin release version $INGEST_VERSION with tag $TRAVIS_TAG";
+#			lein deploy;
+#			lein deploy-uberjar;
+#			./scripts/docker/dockerize.sh;
+#		else
+#			echo "ERROR version/tag mismatch $INGEST_VERSION/$TRAVIS_TAG"; exit 1;
+#		fi;
+#	fi
