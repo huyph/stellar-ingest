c27c147b2830b7fb76277a4f3d418a0d6c1a3d7c
TheDen <deniskhoshaba@gmail.com>
2018-07-26 16:16:59 +1000

    test lein builds

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -6,16 +6,16 @@ INGEST_TAG=$(git describe --tags|head -1 2> /dev/null)
 lein cloverage --coveralls && curl -F 'json_file=@target/coverage/coveralls.json' "https://coveralls.io/api/v1/jobs";
 echo ""; echo ""; echo "branch: $BUILDKITE_BRANCH, version: $INGEST_VERSION, PR: $BUILDKITE_PULL_REQUEST, travis_tag: $BUILDKITE_TAG, ingest_tag: $INGEST_TAG"
 
-if [ "$BUILDKITE_BRANCH" == "devel" ]; then
+#if [ "$BUILDKITE_BRANCH" == "" ]; then
 	if [[ $INGEST_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]]; then
 		echo "Publishing snapshot from devel branch.";
 		lein deploy;
 		lein deploy-uberjar;
-		./scripts/docker/dockerize.sh;
+		#./scripts/docker/dockerize.sh;
 	else
 		echo "ERROR invalid snapshot version."; exit 1;
 	fi;
-fi
+#fi
 
 if [ -n "$BUILDKITE_TAG"  ]; then
 	if [ "$BUILDKITE_TAG" == "v$INGEST_VERSION" ]; then
