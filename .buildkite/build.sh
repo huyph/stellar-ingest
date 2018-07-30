6abc39bc2a6ab1f5ed6d2c5e252d7afa99e88c51
TheDen <deniskhoshaba@gmail.com>
2018-07-30 14:23:00 +1000

    fix INGEST_TAG --> INGEST_VERSION

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 buildkite-agent artifact download target/uberjar/*.jar ./ --step ':clojure: build' --build ${BUILDKITE_BUILD_ID}
 
-INGEST_TAG=$(buildkite-agent meta-data get "ingest-version")
-echo "$INGEST_TAG"
+INGEST_VERSION=$(buildkite-agent meta-data get "ingest-version")
+echo "$INGEST_VERSION"
 
 if [[ $INGEST_VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]]; then
   docker build -f scripts/docker/Dockerfile -t data61/stellar-ingest:snapshot .
